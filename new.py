29cd7dc18ef73b88860ddacdee3a55fdb364ffcf
thenukapavanrathnayaka <161663348+thenukapavanrathnayaka@users.noreply.github.com>
2024-05-30 07:16:02 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,33 @@
+import random
+import json
+import pickle
+import numpy as np
+import tensorflow as tf
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignoreLetters = ['?', '!', '.',',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        wordList = nltk.word_tokenize(pattern)  
+        words.extend(wordList)
+        documents.append((wordList, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+ words = [lemmatizer.lemmatize(word) for word in words if word not in ] 
+ words = sorted(set(classes))
+
+classes= sorted(set(classes))  
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle
